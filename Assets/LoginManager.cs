c9a7a53b9846350867379b704ca34760770a0a34
Pepe08tn <48521743@est.ort.edu.ar>
2024-05-27 15:49:58 -0300

    Añadido la contra de acceso puesta por mi

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+//Cuando el usuario presione el botón BtnAceptar
+//debe escribirse un mensaje en la consola
+//con una contraseña predefinida del
 public class LoginManager : MonoBehaviour
 {
+    public Text txtaceptar;
+    public InputField MiContra;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Contrasena()
+    {
+        if (MiContra.text == "Messi123")
+        {
+            Debug.Log("Accesso granted");
+        }
+        else
+        {
+            Debug.Log("Accesso denied");
+        }
     }
 }
+        
